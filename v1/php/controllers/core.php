5e54848a50c9403795c4181ac3eb9cba31cc83e8
Paulo Vieira <paulo.vieira@agenciavivavida.com.br>
2013-05-30 02:07:17 -0300

    api core controller

---
+++ v1/php/controllers/core.php
@@ -0,0 +1,50 @@
+<?php
+/*
+Controller name: Core
+Controller description: Basic introspection methods
+*/
+
+class JSON_API_Core_Controller {
+  
+ 
+  
+  public function info() {
+
+    global $json_api;
+	
+    $php = '';
+    if (!empty($json_api->query->controller)) {
+      return $json_api->controller_info($json_api->query->controller);
+    } else {
+      $dir = json_api_dir();
+      if (file_exists("$dir/index.php")) {
+        $php = file_get_contents("$dir/index.php");
+      } else {
+        // Check one directory up, in case json-api.php was moved
+        $dir = dirname($dir);
+        if (file_exists("$dir/index.php")) {
+          $php = file_get_contents("$dir/index.php");
+        }
+      }
+      if (preg_match('/^\s*Version:\s*(.+)$/m', $php, $matches)) {
+        $version = $matches[1];
+      } else {
+        $version = '(Unknown)';
+      }
+ 
+
+      $controllers =  $json_api->get_controllers();
+	  
+ 
+      return array(
+        'json_api_version' => $version,
+        'controllers' => array_values($controllers)
+      );
+    }
+  }
+  
+ 
+   
+}
+
+?>
